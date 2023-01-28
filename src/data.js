0dd9de5f8dad263854e037e389ba8a09ec6486c1
Tewogbade <tewogbade3008@gmail.com>
2023-01-28 19:41:27 +0100

    Home page design

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "f5fafd",
+  },
+
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGE WEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg",
+    title: "SHIRT STYLE",
+  },
+
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg",
+    title: "LIGHT JACKET",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+  },
+
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/1462637/pexels-photo-1462637.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/1468380/pexels-photo-1468380.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 5,
+    img: "https://media.istockphoto.com/id/1174942908/photo/female-hand-holding-tote-bag-against-brown-background-empty-space.jpg?s=612x612&w=0&k=20&c=OQVeUjh21I-wJMRKeycXMx5hwPQlxO8uRqvxwVe2soM=",
+  },
+  {
+    id: 6,
+    img: "https://media.istockphoto.com/id/513075727/photo/blue-cap.jpg?s=612x612&w=0&k=20&c=FaOITmcdIbB2Cn3zsGK36uTt99RX8miQST0YQ6Um8hc=",
+  },
+  {
+    id: 7,
+    img: "https://images.pexels.com/photos/6289773/pexels-photo-6289773.jpeg?auto=compress&cs=tinysrgb&w=600",
+  },
+  {
+    id: 8,
+    img: "https://images.pexels.com/photos/1374128/pexels-photo-1374128.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  },
+];
