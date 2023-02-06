9df10ab3225b4c2f30f88fab210e59c1025bc1a8
Tewogbade <tewogbade3008@gmail.com>
2023-02-06 19:27:48 +0100

    Home page design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,8 @@
+import { Facebook, GitHub, Instagram, Twitter } from "@mui/icons-material";
 import styled from "styled-components";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 const Container = styled.div`
   display: flex;
@@ -6,22 +10,125 @@ const Container = styled.div`
 
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+
+const SocialContainer = styled.div`
+  display: flex;
+`;
+
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
 `;
 
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
 `;
 
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+const Payment = styled.img`
+  width: 50%;
 `;
 
 const Footer = () => {
   return (
     <Container>
-      <Left></Left>
-      <Center></Center>
-      <Right></Right>
+      <Left>
+        <Logo>Shop by Tewo.</Logo>
+        <Desc>
+          There are many variations of passangers of Lorem Ipsum avaialable, but
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised words which don't look even slightly believable.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="000000">
+            <GitHub />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+
+        <List>
+          <ListItem>Home </ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Constact</Title>
+        <ContactItem>
+          <LocationOnIcon style={{ marginRight: "10px" }} />
+          Flat 118B, Lorem Ipsum Estate, Adekunle, Yaba. Lagos
+        </ContactItem>
+        <ContactItem>
+          <LocalPhoneIcon style={{ marginRight: "10px" }} />
+          +234 818 973 3799
+        </ContactItem>
+        <ContactItem>
+          <MailOutlineIcon style={{ marginRight: "10px" }} />
+          contact@tewogbade3008@gmail.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
     </Container>
   );
 };
